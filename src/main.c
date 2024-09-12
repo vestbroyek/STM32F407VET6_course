c047b6fcef41493304183da2bcad913fb832dcf9
Maurits <mcb.westbroek@gmail.com>
2024-09-12 08:57:25 +0100

    Use bitfields and structs for LED blinking

--- src/main.c
+++ src/main.c
@@ -6,24 +6,26 @@ void delay(volatile uint32_t count);
 int main(void)
 {
   // Create pointers for main addresses
-  uint32_t* p_clk_ctrl_reg = (uint32_t*) 0x40023830;
-  uint32_t* p_output_mode_reg = (uint32_t*) 0x40020000;
-  uint32_t* p_output_data_reg = (uint32_t*) 0x40020014;
+  RCC_AHB1ENR_t volatile *const p_clk_ctrl_reg = (RCC_AHB1ENR_t*) 0x40023830;
+  GPIOx_MODE_t volatile *const p_output_mode_reg = (GPIOx_MODE_t*) 0x40020000;
+  GPIOx_ODR_t volatile *const p_output_data_reg = (GPIOx_ODR_t*) 0x40020014;
 
   // Enable clock by setting 0th bit at 0x40023830 to high
-  *p_clk_ctrl_reg |= (1 << 0); // GPIOAEN = 1
+  p_clk_ctrl_reg->gpioa_en = 1; // GPIOAEN = 1
 
   // Configure mode of 6th and 7th IO pins as output
-  // 1. First clear it with bitwise AND
-  *p_output_mode_reg &= ~((0b11 << 12) | (0b11 << 14)); // Clear bits 12-15
-  // Then set 12th-13th and 14th-15th bits to 01
-  *p_output_mode_reg |= ((0b01 << 12) | (0b01 << 14)); // Set bits 12-13 and 14-15 to 01
+  p_output_mode_reg->pin_6 = 1;
+  p_output_mode_reg->pin_7 = 1;
 
   while (1) {
     // Finally set the 6th and 7th bit of the output data register to high
-    *p_output_data_reg |= (1 << 6) | (1 << 7); // Set bits 6 and 7 to high
+    p_output_data_reg->pin_6 = 1;
     delay(1000000);
-    *p_output_data_reg &= ~((1 << 6) | (1 << 7)); // Turn LEDs off
+    p_output_data_reg->pin_6 = 0;
+    delay(1000000);
+    p_output_data_reg->pin_7 = 1;
+    delay(1000000);
+    p_output_data_reg->pin_7 = 0;
     delay(1000000);
   };
 
